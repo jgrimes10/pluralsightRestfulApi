5cc4492181a0606e42e566ae2a1286d0b2df836f
jgrimes10 <uY2&J4fdPi5#>
2019-04-09 21:42:04 -0400

    Add services. Update packages

---
+++ PluralsightRESTfulAPI/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluralsightRESTfulAPI.Helpers
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+        {
+            var currentDate = DateTime.UtcNow;
+            var age = currentDate.Year - dateTimeOffset.Year;
+
+            if (currentDate < dateTimeOffset.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
